7c25604e29828f3581c7252c91e41739035f64b6
FOSDEM Stands Team <stands@fosdem.org>
2021-02-01 20:01:36 +0000

    UPdate script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-cd /var/opt/app/fosdem_submission/html/stands-website
+parent="/var/opt/app/fosdem_submission/html/stands-website"
+
+cd "$parent"
 
 git pull origin master
 
-git submodule init
-git submodule update
+#git submodule init
+#git submodule update
 
-git pull --recurse-submodules
+#git pull --recurse-submodules
+
+
+for d in $(find content/stands -maxdepth 1 -type d); do
+
+	basename=$(basename "$d")
+	if [ "$basename" != "stands" ]; then
+		cd "$d"
+		git branch > /dev/null
+		echo "$d"
+		if [ "$?" == "0" ]; then
+			git pull origin $(git branch | tail -1 | sed 's/*//' | awk '{print $1}')
+		fi
+		cd "$parent"
+	fi
+
+done
 
 hugo
 
