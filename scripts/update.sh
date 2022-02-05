6fd8f0a222edcccbc5ccddb1096c97eeda2adee4
Pieter De Praetere <pieter.de.praetere@helptux.be>
2022-02-05 09:04:25 +0100

    Fix the fix scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -14,7 +14,19 @@ git pull --no-edit origin master
 #git submodule sync
 #git submodule update
 git submodule sync
+if [ "$?" -ne 0 ]; then
+  git submodule update --init --force --remote
+fi
 git submodule update
+
+if [ "$?" -ne 0 ]; then
+  git submodule update --init --force --remote
+fi
+
+if [ "$?" -ne 0 ]; then
+  exit $?
+fi
+
 hugo
 
 exit 0
